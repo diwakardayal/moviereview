741ff247f8ae0cb2aa81c9e31ddd2975b8bb715e
diwakardayal <diwakardayal65@gmail.com>
2024-03-22 21:10:56 +0530

    add admin header; minor fixes;

---
+++ frontend/src/admin/Header.jsx
@@ -0,0 +1,87 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useRef, useState } from "react"
+import { AiOutlinePlus } from "react-icons/ai"
+import { BsFillSunFill } from "react-icons/bs"
+import { useTheme } from "../hooks"
+
+export default function Header() {
+	const [show, setShow] = useState(false)
+	const { toggleTheme } = useTheme()
+
+	return (
+		<div className="flex items-center justify-between relative">
+			<input
+				type="text"
+				className="border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary dark:text-white transition bg-transparent rounded text-lg p-1 outline-none"
+				placeholder="Search Movies..."
+			/>
+
+			<div className="flex items-center space-x-3">
+				<button onClick={toggleTheme}>
+					<BsFillSunFill className="dark:text-dark-subtle " size={24} />
+				</button>
+				<button
+					className="dark:text-dark-subtle flex items-center space-x-2 border-secondary dark:border-dark-subtle  hover:border-primary text-secondary hover:opacity-80 transition font-semibodl border-2 rounded text-lg px-3 py-1"
+					onClick={() => setShow(true)}
+				>
+					<span>Create</span>
+					<AiOutlinePlus />
+				</button>
+
+				<CreateOptions isVisible={show} onClose={() => setShow(false)} />
+			</div>
+		</div>
+	)
+}
+
+const CreateOptions = ({ isVisible, onClose }) => {
+	const container = useRef()
+	const containerID = "options-container"
+
+	useEffect(() => {
+		const handleClose = e => {
+			if (!isVisible) return
+			const { parentElement, id } = e.target
+
+			if (parentElement.id === containerID || id === containerID) return
+
+			if (container.current) {
+				if (!container.current.classList.contains("animate-show-in"))
+					container.current.classList.add("animate-show-out")
+			}
+		}
+
+		document.addEventListener("click", handleClose)
+		return () => {
+			document.removeEventListener("click", handleClose)
+		}
+	}, [isVisible])
+
+	if (!isVisible) return null
+
+	return (
+		<div
+			id={containerID}
+			ref={container}
+			className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded animate-show-in"
+			onAnimationEnd={e => {
+				if (e.target.classList.contains("animate-show-out")) onClose()
+				e.target.classList.remove("animate-show-in")
+			}}
+		>
+			<Option>Add Movie</Option>
+			<Option>Add Actor</Option>
+		</div>
+	)
+}
+
+const Option = ({ children, onClick }) => {
+	return (
+		<button
+			className="dark:text-white text-secondary hover:opacity-80 transition"
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	)
+}
--- frontend/src/navigator/AdminNavigator.jsx
+++ frontend/src/navigator/AdminNavigator.jsx
@@ -4,19 +4,23 @@ import MoviesPage from "../Pages/MoviesPage"
 import ActorsPage from "../admin/ActorsPage"
 import NotFoundPage from "../Pages/404Page"
 import Dashboard from "../admin/Dashboard"
+import Header from "../admin/Header"
 
 export default function AdminNavigator() {
 	return (
-		<div className="flex">
-			<NavBar />
-			<div className="flex-1 p-2 max-w-screen-xl">
-				<Routes>
-					<Route path="/" element={<Dashboard />} />
-					<Route path="/movies" element={<MoviesPage />} />
-					<Route path="/actors" element={<ActorsPage />} />
-					<Route path="/*" element={<NotFoundPage />} />
-				</Routes>
+		<>
+			<div className="flex dark:bg-primary bg-white">
+				<NavBar />
+				<div className="flex-1 p-2 max-w-screen-xl">
+					<Header />
+					<Routes>
+						<Route path="/" element={<Dashboard />} />
+						<Route path="/movies" element={<MoviesPage />} />
+						<Route path="/actors" element={<ActorsPage />} />
+						<Route path="/*" element={<NotFoundPage />} />
+					</Routes>
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
