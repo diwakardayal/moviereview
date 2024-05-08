6afcefede54f72017966036e6c7bc58b02c39176
diwakardayal <diwakardayal65@gmail.com>
2024-05-08 17:54:17 +0530

    add: console log to asynchandle middleware;

--- src/middleware/asyncHandler.js
+++ src/middleware/asyncHandler.js
@@ -1,4 +1,8 @@
 const asyncHandler = fn => (req, res, next) => {
-	Promise.resolve(fn(req, res, next)).catch(next)
+	Promise.resolve(fn(req, res, next)).catch(err => {
+		console.error("Async error caught:", err)
+		next(err)
+	})
 }
+
 module.exports = asyncHandler
